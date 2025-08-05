047de2a7c85ca319dbe7cc1b49b3d7d6ccb33cd5
HoaiTam-K18 <23521394@gm.uit.edu.vn>
2025-08-05 10:43:59 +0700

    Add Bronze Layer: raw text asset for quake data

--- etl_pipeline/etl_pipeline/definitions.py
+++ etl_pipeline/etl_pipeline/definitions.py
@@ -1,9 +1,21 @@
 from dagster import Definitions, load_assets_from_modules
 
-from etl_pipeline import assets  # noqa: TID252
+from .assets.bronze import bronze__quake_raw_text
 
-all_assets = load_assets_from_modules([assets])
+from .resources.minio_io_manager import MinIOIOManager
+
+all_assets = load_assets_from_modules([bronze__quake_raw_text])
+
+MINIO_CONFIG = {
+    "endpoint_url": "localhost:9000",
+    "bucket": "warehouse",
+    "aws_access_key_id": "minioadmin",
+    "aws_secret_access_key": "minioadmin123",
+}
 
 defs = Definitions(
     assets=all_assets,
+    resources={
+        "minio_io_manager": MinIOIOManager(MINIO_CONFIG),
+    },
 )
